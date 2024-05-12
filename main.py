f5634ab8615289d914bd34b251eb6c1906f8717c
Alves0611 <g.alves0611@gmail.com>
2024-05-11 22:58:22 -0300

    get data from uf, city, neighborhood

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import requests
 
-link = 'https://viacep.combr/ws/01001000/json'
+cep = '20090002'
+
+link = f'https://viacep.com.br/ws/01001000/json'
+
+request = requests.get(link)
+
+uf = request_dic['uf']
+city = request_dic['localidade']
+neighborhood = request_dic['bairro']
+print(uf, city, neighborhood)
